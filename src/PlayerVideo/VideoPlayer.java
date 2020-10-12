800d46fa62baba6ca9f3bdf9fefb9e65b0329f3c
Mario_junior23 <mariojunior3251@gmail.com>
2020-10-12 00:36:42 -0300

    Adicionando componente de acesso player

--- src/PlayerVideo/VideoPlayer.java
+++ src/PlayerVideo/VideoPlayer.java
@@ -1,5 +1,15 @@
 package PlayerVideo;
 
-public class VideoPlayer {
+import javax.swing.JFrame;
 
-}
+import uk.co.caprica.vlcj.player.component.EmbeddedMediaPlayerComponent;
+
+public class VideoPlayer {
+	
+	public static void main(String[] args) {
+		
+		EmbeddedMediaPlayerComponent Component = new EmbeddedMediaPlayerComponent();
+ m
+	}
+    
+ }
